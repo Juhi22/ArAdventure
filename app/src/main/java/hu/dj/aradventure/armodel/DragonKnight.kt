04d0aa252483fe0eaa01bc6c583de768400c8536
Juhi22 <j.dani22@freemail.hu>
2024-08-20 10:41:42 +0200

    Add dragon knight sounds

--- app/src/main/java/hu/dj/aradventure/armodel/DragonKnight.kt
+++ app/src/main/java/hu/dj/aradventure/armodel/DragonKnight.kt
@@ -33,9 +33,9 @@ object DragonKnight: Enemy() {
     override val sounds: Map<String, String>
         get() {
             return mutableMapOf(
-                "default" to "dragon_knight/sounds/1.mp3",
-                "attack/1" to "dragon_knight/sounds/1.mp3",
-                "dead/1" to "dragon_knight/sounds/1.mp3",
+                "default" to "dragon_knight/sounds/dragon_knight_attack.mp3",
+                "attack/1" to "dragon_knight/sounds/dragon_knight_attack.mp3",
+                "dead/1" to "dragon_knight/sounds/dragon_knight_dead.mp3",
             )
         }
 
